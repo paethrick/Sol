f96ed5868943e1217bb0ab782ad3221bb463f287
erik <80585437+paethrick@users.noreply.github.com>
2023-03-09 23:55:55 +0100

    : enhanc: updated flat page
    routing working on flat page, added some css
    
    #6

--- Sol/src/FlatPage/FlatPage.jsx
+++ Sol/src/FlatPage/FlatPage.jsx
@@ -1,7 +1,56 @@
+<<<<<<< Updated upstream
 export function FlatPage({ flat }){
     return(
         <div>
             {flat.owner.name}
+=======
+import { useLocation, useParams, Link } from 'react-router-dom'
+import Sol_Picture from '/Sol_Picture.png'
+import { useEffect, useState } from 'react'
+
+export function FlatPage(){
+
+    const [flat,setFlat] = useState()
+    useEffect(()=>{
+      setFlat(location)
+    },[])
+    const location = useLocation()
+    console.log(location)
+    
+    return(
+        <div>
+            {/* Logo */}
+            <div className='flex w-auto justify-center' >
+            <Link to="/"><img src={Sol_Picture} alt="Sol"
+                className='w-48'/></Link>
+            </div>
+            {/* Name */}
+            <div className='flex-wrap pl-20 mb-4'>
+                <h1 className='flex text-3xl'>{location.state.listingName}</h1>
+                <h2 className='flex text-2xl'>{location.state.publicAddress}</h2>
+            </div>
+            {/* Picture Slide */}
+            <div className='flex flex-nowrap justify-center '>
+              <div className='flex object-cover'>
+                <img className='relative' src={location.state.images[0]}/>
+              </div>
+              <div className='flex flex-wrap w-[45rem]'>
+                {location.state.images.map((e,index)=>{
+                  return index < 4 && index != 0 ? <img key={index} className='w-[22.5rem] h-[15rem] object-cover' src={e}/> : null
+                })}
+              </div>
+            </div>
+            {/* Spec */}
+            <div className='flex-nowrap ml-20 mt-4'>
+              <div className='flex gap-10'>
+                <p className='text-xl'>{location.state.accessibilityLabel}</p>
+                <p className='flex gap-1 text-mg align-middle'>&#9733; {location.state.avgRatingLocalized}</p>
+              </div>
+              <div className='flex text-md gap-2'>
+                {location.state.listingGuestLabel} | {location.state.roomType} | {location.state.listingBedLabel} {location.state.listingBathroomLabel} 
+              </div>
+            </div>
+>>>>>>> Stashed changes
         </div>
     )
 }
---
+++ Sol/src/MainPage/MainPage.jsx
@@ -0,0 +1,42 @@
+import { useState,useEffect } from "react";
+import { FlatCard } from "./FlatCard";
+import { Link } from "react-router-dom";
+import Sol_Picture from '/Sol_Picture.png'
+
+export const MainPage = () => {
+    const [flats,setFlat] = useState([])
+    useEffect(()=>{
+      getThis()
+    },[])
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'b339facf5amsh2d95b4cac3039a7p19abbejsnc843ac7a1f19',
+        'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com',
+        'User-agent': 'your bot 0.1'
+      }
+    };
+    
+    
+    const getThis = async () => {
+      const response = await fetch('https://airbnb19.p.rapidapi.com/api/v1/searchProperty?category=TAB_8225&totalRecords=18&currency=USD&adults=1', options)
+      const data = await response.json()
+      setFlat(data.data)
+    }
+
+    return(
+      <div>
+        {/* Logo */}
+        <div className='flex w-auto justify-center' >
+            <Link to="/"><img src={Sol_Picture} alt="Sol"
+                className='w-48'/></Link>
+            </div>
+        <div className="flex justify-center">
+        <div className="flex flex-wrap w-auto gap-x-7 justify-center mx-7">
+            {flats.length>1 ? flats.map((e,i)=><FlatCard getThis={getThis} flats={flats[i]} key={i} index={i}/>) : <h1>Loading...</h1>}
+        </div>
+        </div>
+      </div>
+    )
+}
