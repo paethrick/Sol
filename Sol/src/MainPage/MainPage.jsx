2d331faf5398275f98531082f728cd70143c5d81
erik <80585437+paethrick@users.noreply.github.com>
2023-03-12 12:27:55 +0100

    : enhance: nav bar navigation
    #16

--- Sol/src/MainPage/MainPage.jsx
+++ Sol/src/MainPage/MainPage.jsx
@@ -1,44 +1,107 @@
-import { useState } from "react";
-import { FlatCard } from "./FlatCard";
+import { useState,useEffect } from "react";
+import { PropertyCard } from "./PropertyCard";
 import { NavBar } from "../components/NavBar";
 import Sol_Picture from '/Sol_Picture.png'
 import { SearchBar } from "../components/SearchBar";
+import { Link } from "react-router-dom";
 
 
 export const MainPage = () => {
-    const [flats,setFlat] = useState([])
+    const [property, setProperty] = useState([])
+    const [search, setSearch] = useState([])
+    const [category, setCategory] = useState('')
 
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'b339facf5amsh2d95b4cac3039a7p19abbejsnc843ac7a1f19',
-        'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+    useEffect(()=>{
+      if(property.length<1){
+        starterPage()
       }
-    };
-    
-    const getThis = async () => {
-      const response = await fetch('https://airbnb19.p.rapidapi.com/api/v1/searchProperty?category=TAB_8225&totalRecords=30&currency=USD&adults=1', options)
+    },[])
+
+    // STARTER PAGE
+    const starterPage = async () => {
+
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b339facf5amsh2d95b4cac3039a7p19abbejsnc843ac7a1f19',
+		      'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+        }
+      };
+
+      const response = await fetch(`https://airbnb19.p.rapidapi.com/api/v1/searchProperty?category=TAB_8225&totalRecords=10&currency=USD&adults=1`, options)
       const data = await response.json()
-      setFlat(data.data)
+      setProperty(data.data)
+    }
+    
+    // SEARCH PROPERTY BY PLACE
+    const searchProperty = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b339facf5amsh2d95b4cac3039a7p19abbejsnc843ac7a1f19',
+          'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+        }
+      };
+
+      const response = await fetch(`https://airbnb19.p.rapidapi.com/api/v1/searchPropertyByPlace?id=${search.destId}&display_name=${search.destName}%2C%20${search.destId}&totalRecords=18&currency=USD&adults=1`, options)
+      const data = await response.json()
+      setProperty(data.data)
+    }
+    
+    const searchDestination = async (input) => {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b339facf5amsh2d95b4cac3039a7p19abbejsnc843ac7a1f19',
+          'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+        }
+      };
+
+      const response = await fetch(`https://airbnb19.p.rapidapi.com/api/v1/searchDestination?query=${input}`,options)
+      const data = await response.json()
+      const pack = {
+        'destId' : data.data[0].id,
+        'countryCode' : data.data[0].countryCode,
+        'destName' : data.data[0].display_name
+      }
+      setSearch(pack)
+    }
+
+    // GET CATEGORY
+    const getCategory = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'b339facf5amsh2d95b4cac3039a7p19abbejsnc843ac7a1f19',
+          'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+        }
+      };
+
+      const response = await fetch('https://airbnb19.p.rapidapi.com/api/v1/getCategory', options)
+      const data = await response.json()
+      const limit = 10
+      const limitedData = data.data.slice(0,limit)
+      setCategory(limitedData)
     }
 
     return(
       <div>
+        <button onClick={searchProperty}>Press</button>
+        <br />
+        <button onClick={getCategory}>Category</button>
         <div className="flex justify-between">
           {/* Logo */}
           <div>
                   <img src={Sol_Picture} alt="Sol"
                   className='w-24'/>
           </div>
-          <SearchBar/>
+          <SearchBar searchDestination={searchDestination} category={category}/>
           <p></p>
         </div>
-        <NavBar />
-
-        <button onClick={getThis}>Press</button>
+        <NavBar category={category}/>
         <div className="flex justify-center">
         <div className="flex flex-wrap w-auto gap-x-7 justify-center mx-7">
-            {flats.length>1 ? flats.map((e,i)=><FlatCard getThis={getThis} flats={flats[i]}/>) : null}
+            {property ? property.map((e,i)=><PropertyCard property={property[i]} key={i}/>) : null}
         </div>
         </div>
       </div>
