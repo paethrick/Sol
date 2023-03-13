885a4c33115979115ec2ffb242b43efbcb0b49f3
erik <80585437+paethrick@users.noreply.github.com>
2023-03-13 20:49:44 +0100

    enhance: updated API call logic - print properties
    probably the best solution with this limited API
    
    # 18

--- Sol/src/RoadTripPage/RoadTripPage.jsx
+++ Sol/src/RoadTripPage/RoadTripPage.jsx
@@ -4,8 +4,17 @@ import { PropertyCard } from '../MainPage/PropertyCard';
 
 export const RoadTripPage = () => {
 
-  const [search,setSearch] = useState(false)
+  const [search,setSearch] = useState([])
+  const [toggle, setToggle] = useState(false)
   const [property, setProperty] = useState([])
+
+  const printOut = () => {
+    setToggle(true)
+  }
+
+  let otherLength = 0
+  let theLength = 0
+  const propertyGroup = []
 
   // SEARCH PROPERTY BY PLACE
   const searchPropertyByPlace = async (input) => {
@@ -22,25 +31,24 @@ export const RoadTripPage = () => {
     console.log(data.data)
     const something = data.data
     setProperty(oldArray =>[...oldArray,something])
-    console.log(property)
+    otherLength+=1
+    if(otherLength == theLength){
+      printOut()
+    }
   }
 
-const printOut = () => {
-  console.log(property)
-}
-
-const startSearch = (input) => {
-  const tryThis = input[0]
-  for(let i=0; i<tryThis.length; i++) {
-    setTimeout(()=>{
-      searchDestination(tryThis[i])
-    },i * 4000);
+  const startSearch = (input) => {
+    const payload = input[0]
+    theLength = payload.length
+    for(let i=0; i<payload.length; i++) {
+      setTimeout(()=>{
+        searchDestination(payload[i],payload.length)
+      },i * 1500);
+    }
   }
-}
-
 
   // SEARCH DESTINATION
-  const searchDestination = async (input) => {
+  const searchDestination = async (input, number) => {
     const options = {
       method: 'GET',
       headers: {
@@ -59,22 +67,32 @@ const startSearch = (input) => {
         'destName' : data.data[0].display_name
       }
     
-      //setSearch([...search,pack])
-      setTimeout(()=>{
-        searchPropertyByPlace(pack)
-      },6000)
+      //setSearch(elem => [...elem,pack])
       
-  }
+      propertyGroup.push(pack)
+      propertyGroup.length == number ? setTimeout(()=>{propertyDispenser()},1500) : null
+    }
+
+    const propertyDispenser = () => {
+        for(let i=0; i<propertyGroup.length; i++) {
+          setTimeout(()=>{
+            searchPropertyByPlace(propertyGroup[i])
+          },i * 1500);
+        }
+      }
+
+      {/* {toggle ? property.map((elem,i) => elem
+              .map((e,i)=><PropertyCard property={property[i]} index={i} key={i}/>)): null} */}
+      
 
   return(
     <div>
     <div>
-      <button onClick={printOut}> Print </button>
       <SearchPanel propertyList={startSearch}/>
     </div>
     <div className="flex justify-center">
         <div className="flex flex-wrap w-auto gap-x-7 justify-center mx-7">
-            {search ? property[0].map((e,i)=><PropertyCard property={property[i]} index={i} key={i}/>) : null}
+            { toggle ? property.map((e,ind)=> e.map((k,i) =><PropertyCard property={property.at(ind)[i]} index={i} key={i}/>)): null}
         </div>
         </div>
     </div>
